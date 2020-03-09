3ffcbf1400a7962606f1cde5459674367ab9258e
Mayur <mayur.bode95@gmail.com>
2020-03-09 10:17:25 +0000

    Update demo.java

--- demo.java
+++ demo.java
@@ -4,11 +4,12 @@ public static void main(String arg[])
 {
 System.out.println("Demo");
   System.out.println("Demo....2");
+}
 public void print()
 {
    System.out.println("print in Demo");
 }
-}
+
   public void show()
   {
     System.out.println("Show in Demo");
