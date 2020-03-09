0a23afdb87ba89c17c414f6e38ffaff5bf75b86e
Mayur <mayur.bode95@gmail.com>
2020-03-09 07:12:29 +0000

    Show method is added

--- demo.java
+++ demo.java
@@ -1,7 +1,13 @@
 public class Demo
+{
 public static void main(String arg[])
 {
 System.out.println("Demo");
   System.out.println("Demo....2");
 
+}
+  public void show()
+  {
+    System.out.println("Show in Demo");
+  }
 }
